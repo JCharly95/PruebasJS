ef45f9007861197bbf203af8f51bfc315cc3bf40
JCharly95 <33488657+JCharly95@users.noreply.github.com>
2022-09-15 12:22:48 -0500

    Prueba de Chart.js
    En estos archivos se uso de referencia un ejemplo para crear una grafica de barras, pero los titulares y colores (de cada muestra) se obtuvieron de manera "dinamica" al revisar el arreglo de valores, asi como elaborar una funcion para la obtencion de colores RGB random.

---
+++ ChartJS_Pruebas/js/randomRGB.js
@@ -0,0 +1,14 @@
+function numRandom(numero){
+    return (Math.random()*numero).toFixed(0);
+}
+
+function randColorRGB(){
+    let color = "("+numRandom(255)+","+numRandom(255)+","+numRandom(255)+")";
+    return "rgb" + color;
+}
+
+function valorRandom(cant){
+    let valores = [];
+
+    
+}
